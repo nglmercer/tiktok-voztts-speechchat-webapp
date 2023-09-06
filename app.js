c70257dd197b56711c3b23dc24b92a8dc29c8dee
nglmercer <128845117+nglmercer@users.noreply.github.com>
2023-09-05 22:47:12 -0500

    Add files via upload
    actualizacion

--- app.js
+++ app.js
@@ -8,9 +8,6 @@ let viewerCount = 0;
 let likeCount = 0;
 let diamondsCount = 0;
 
-
-
-
 // These settings are defined by obs.html
 if (!window.settings) window.settings = {};
 
@@ -228,54 +225,132 @@ connection.on('streamEnd', () => {
         }, 30000);
     }
 })
-let lastMessage = '';
-let repeatCount = 0;
+let messageList = []; // Lista de mensajes
+let isReading = false; // Flag to track if a message is currently being read
+let messageCount = 0; // Counter to keep track of messages
+let repeatedMessageCount = 0; // Counter to keep track of repeated messages
+const forbiddenWords = ['joined','followed the LIVE creator', 'shared the live']; // Palabras que no se deben leer en voz alta
 
-function readMessageInVoice(message) {
-  const speechMsg = new SpeechSynthesisUtterance();
-  speechMsg.text = message;
-  const voices = speechSynthesis.getVoices();
-  const spanishFemaleVoice = voices.find(voice => voice.lang === 'es-ES' && voice.name === 'Google español de España');
-  speechMsg.voice = spanishFemaleVoice;
-  speechMsg.lang = 'es-ES';
+function changeVoice() {
+  const voice = document.querySelector("select").value;
+  const rate = document.querySelector("input[type=range]").value;
+  const volume = 1;
+  responsiveVoice.setDefaultVoice(voice);
 
-  // Adjust the rate based on the length of the message and whether it has been repeated
-  if (message === lastMessage) {
-    repeatCount++;
-    if (repeatCount >= 3) {
-      speechMsg.rate = 2.0;
-    } else {
-      speechMsg.rate = 1.5;
-    }
-  } else {
-    repeatCount = 0;
-    if (message.length < 5) {
-      speechMsg.rate = 0.5;
-    } else if (message.length < 10) {
-      speechMsg.rate = 1.0;
-    } else {
-      speechMsg.rate = 1.5;
-    }
+  // Check if a message is currently being read
+  if (!isReading) {
+    readNextMessage(rate, volume);
   }
-
-  // Exclude certain words from being read out loud
-  const filteredMessage = message.replace(/joined|badword2|badword3/gi, '');
-  speechMsg.text = filteredMessage;
-
-  speechSynthesis.speak(speechMsg);
-
-  lastMessage = message;
 }
 
-// Manejar el clic en el botón de reproducir
-playButton.addEventListener('click', () => {
-  const chatMessages = Array.from(chatContainer.children).map(element => element.textContent);
-  const concatenatedMessages = chatMessages.join('. ');
-  readMessageInVoice(concatenatedMessages);
-});
+function readNextMessage(rate, volume) {
+  if (messageList.length > 0) {
+    isReading = true;
+    const nextMessage = messageList.shift(); // Get and remove the next message from the queue
+    responsiveVoice.speak(nextMessage, null, {
+      rate: rate,
+      volume: volume,
+      onend: function() {
+        isReading = false;
+        readNextMessage(rate, volume); // Continue with the next message
+      }
+    });
 
-connection.on('chat', (msg) => {
-  if (window.settings.showChats === "0") return;
+    messageCount++;
+    
+    // Check if this is the 50th message
+    if (messageCount % 50 === 0) {
+      // If any of the last 50 messages were repeated, adjust the rate to 2
+      if (repeatedMessageCount > 0) {
+        const doubleSpeedRate = 2;
+        changeVoice(doubleSpeedRate);
+      } else {
+        changeVoice(rate);
+      }
+      
+      // Reset the repeated message count
+      repeatedMessageCount = 0;
+    } else {
+      changeVoice(rate);
+    }
+  }
+}
 
-  addChatItem('', msg, msg.comment);
-});
+function readMessageInVoice(message, voice) {
+  if (!message || message.length < 2) {
+    // El mensaje es nulo o tiene menos de 2 caracteres, no se leerá en voz alta
+    return;
+  }
+
+  const cleanMessage = cleanMessageWithForbiddenWords(message);
+
+  if (cleanMessage.trim() === '') {
+    // El mensaje no contiene caracteres legibles, no se leerá en voz alta
+    return;
+  }
+
+  addMessage(cleanMessage); // Agregar el mensaje a la lista de mensajes
+}
+
+function cleanMessageWithForbiddenWords(message) {
+  let cleanMessage = message;
+
+  // Remover acentuación y símbolos, pero mantener otros caracteres de idiomas diferentes
+  const specialCharsRegex = /[^a-zA-Z0-9áéíóúÁÉÍÓÚñÑüÜ\s\u3000-\u303F\u3040-\u309F\u30A0-\u30FF\uFF00-\uFFEF]/g;
+  const specialCharsCount = (cleanMessage.match(specialCharsRegex) || []).length;
+
+  if (specialCharsCount > 10) {
+    // Se repiten más de 10 veces los caracteres especiales, se elimina el mensaje
+    cleanMessage = '';
+  } else {
+    // Remover palabras prohibidas
+    for (const forbiddenWord of forbiddenWords) {
+      cleanMessage = cleanMessage.replace(new RegExp(forbiddenWord, 'gi'), '');
+    }
+
+    // Remover caracteres especiales
+    cleanMessage = cleanMessage.replace(specialCharsRegex, '');
+  }
+
+  return cleanMessage;
+}
+
+function addMessage(message) {
+  messageList.push(message);
+  // Check if the message is a repeat
+  if (messageList.slice(-50, -1).includes(message)) {
+    repeatedMessageCount++;
+  }
+  // Automatically start reading messages when added to the queue
+  if (!isReading) {
+    const rate = calculateSpeechRate(message);
+    changeVoice(rate);
+  }
+}
+
+function calculateSpeechRate(message) {
+  // Calculate the desired duration based on the message length
+  const messageLength = message.length;
+  let desiredDuration = 4; // Default duration for shorter messages
+  if (messageLength > 15) {
+    desiredDuration = 12; // Adjusted duration for longer messages
+  }
+
+  // Calculate the speech rate to achieve the desired duration
+  const rate = messageLength / desiredDuration;
+  return rate;
+}
+
+window.onload = function() {
+  const voices = responsiveVoice.getVoices();
+  const select = document.querySelector("select");
+
+  for (const voice of voices) {
+    if (voice.lang === "es-ES") {
+      const option = document.createElement("option");
+      option.value = voice.name;
+      option.textContent = voice.name;
+      select.appendChild(option);
+    }
+  }
+}
