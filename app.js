682b59707194510d70d071d04aab1e84f87e3d4b
melserngl <128845117+nglmercer@users.noreply.github.com>
2023-10-28 23:49:58 -0500

    twitch tiktok voz tts web app
    nuevos apis voz speechchat
    streamelements api voice
    
    adaptable interfaz

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ if (!window.settings) window.settings = {};
 
 $(document).ready(() => {
     $('#connectButton').click(connect);
-    $('#uniqueIdInput').on('keyup', function (e) {
+    $('#uniqueIdInput').on('keyup', function(e) {
         if (e.key === 'Enter') {
             connect();
         }
@@ -99,7 +99,7 @@ function addChatItem(color, data, text, summarize) {
     container.animate({
         scrollTop: container[0].scrollHeight
     }, 400);
-    hablarMensaje(text);
+    leerMensajes(text);
     cacheMessage(text);
 }
 
@@ -143,7 +143,7 @@ function addGiftItem(data) {
     } else {
         container.append(html);
     }
-    
+
     container.stop();
     container.animate({
         scrollTop: container[0].scrollHeight
@@ -152,22 +152,22 @@ function addGiftItem(data) {
 
 // Cambiar la posición de los contenedores en función de la relación de aspecto
 $(window).on('resize', function() {
-  let aspectRatio = $(window).width() / $(window).height();
-  if (aspectRatio <= 1) {
-      $('.splitchattable').css('flex-direction', 'column');
-      $('.chatcontainer, .giftcontainer').css('float', 'left');
-      $('.chatcontainer, .giftcontainer').css('width', '100%');
-      $('#roomStats').css('text-align', 'left');
-      $('.chatcontainer').css('padding-right', '0px');
-      $('.giftcontainer').css('padding-left', '0px');
-  } else {
-      $('.splitchattable').css('flex-direction', 'row');
-      $('.chatcontainer, .giftcontainer').css('float', 'none');
-      $('.chatcontainer, .giftcontainer').css('width', 'auto');
-      $('#roomStats').css('text-align', 'center');
-      $('.chatcontainer').css('padding-right', '10px');
-      $('.giftcontainer').css('padding-left', '10px');
-  }
+    let aspectRatio = $(window).width() / $(window).height();
+    if (aspectRatio <= 1) {
+        $('.splitchattable').css('flex-direction', 'column');
+        $('.chatcontainer, .giftcontainer').css('float', 'left');
+        $('.chatcontainer, .giftcontainer').css('width', '100%');
+        $('#roomStats').css('text-align', 'left');
+        $('.chatcontainer').css('padding-right', '0px');
+        $('.giftcontainer').css('padding-left', '0px');
+    } else {
+        $('.splitchattable').css('flex-direction', 'row');
+        $('.chatcontainer, .giftcontainer').css('float', 'none');
+        $('.chatcontainer, .giftcontainer').css('width', 'auto');
+        $('#roomStats').css('text-align', 'center');
+        $('.chatcontainer').css('padding-right', '10px');
+        $('.giftcontainer').css('padding-left', '10px');
+    }
 }).resize();
 
 // estadísticas de espectadores
@@ -180,9 +180,9 @@ connection.on('roomUser', (msg) => {
 
 // estadísticas de likes
 connection.on('like', (msg) => {
-  if (typeof msg.totalLikeCount === 'number') {
-    likeCount = msg.totalLikeCount;
-    updateRoomStats();
+    if (typeof msg.totalLikeCount === 'number') {
+        likeCount = msg.totalLikeCount;
+        updateRoomStats();
 
     }
 })
@@ -241,100 +241,392 @@ connection.on('streamEnd', () => {
         }, 30000);
     }
 })
+var audio, chatbox, button, channelInput, audioqueue, isPlaying, add, client, skip;
+
+const TTS_API_ENDPOINT = 'https://api.streamelements.com/kappa/v2/speech?'; // unprotected API - use with caution
+const PRONOUN_API_ENDPOINT = 'https://pronouns.alejo.io/api/users/';
+const maxMsgInChat = 2 * 10;
+const DESCENDING = true; // newest on top
+const VOICE_PREFIX = '&';
+const pronoun_DB = {}; // username -> pronound_id
+const FEM_PRONOUNS = ['sheher', 'shethey'];
+var CHANNEL_BLACKLIST = [
+    'streamlabs',
+    'streamelements',
+    'moobot',
+    'nightbot',
+    'ch4tsworld',
+    'streamstickers',
+    'laia_bot',
+    'soundalerts',
+    'ankhbot',
+    'phantombot',
+    'wizebot',
+    'botisimo',
+    'coebot',
+    'deepbot',
+];
+var VOICE_LIST = {
+    "Miguel (Spanish, American)": "Miguel",
+    "Penélope (Spanish, American)": "Penelope",
+    "Brian (English, British)": "Brian",
+    "Amy (English, British)": "Amy",
+    "Emma (English, British)": "Emma",
+    "Geraint (English, Welsh)": "Geraint",
+    "Russell (English, Australian)": "Russell",
+    "Nicole (English, Australian)": "Nicole",
+    "Joey (English, American)": "Joey",
+    "Justin (English, American)": "Justin",
+    "Matthew (English, American)": "Matthew",
+    "Ivy (English, American)": "Ivy",
+    "Joanna (English, American)": "Joanna",
+    "Kendra (English, American)": "Kendra",
+    "Kimberly (English, American)": "Kimberly",
+    "Salli (English, American)": "Salli",
+    "Raveena (English, Indian)": "Raveena",
+    "Zhiyu (Chinese, Mandarin)": "Zhiyu",
+    "Mads (Danish)": "Mads",
+    "Naja (Danish)": "Naja",
+    "Ruben (Dutch)": "Ruben",
+    "Lotte (Polly) (Dutch)": "Lotte",
+    "Mathieu (French)": "Mathieu",
+    "Céline (French)": "Celine",
+    "Chantal (French, Canadian)": "Chantal",
+    "Hans (German)": "Hans",
+    "Marlene (German)": "Marlene",
+    "Vicki (German)": "Vicki",
+    "Aditi (+English) (Hindi)": "Aditi",
+    "Karl (Icelandic)": "Karl",
+    "Dóra (Icelandic)": "Dora",
+    "Carla (Italian)": "Carla",
+    "Bianca (Italian)": "Bianca",
+    "Giorgio (Italian)": "Giorgio",
+    "Takumi (Japanese)": "Takumi",
+    "Mizuki (Japanese)": "Mizuki",
+    "Seoyeon (Korean)": "Seoyeon",
+    "Liv (Norwegian)": "Liv",
+    "Ewa (Polish)": "Ewa",
+    "Maja (Polish)": "Maja",
+    "Jacek (Polish)": "Jacek",
+    "Jan (Polish)": "Jan",
+    "Ricardo (Portuguese, Brazilian)": "Ricardo",
+    "Vitória (Portuguese, Brazilian)": "Vitoria",
+    "Cristiano (Portuguese, European)": "Cristiano",
+    "Inês (Portuguese, European)": "Ines",
+    "Carmen (Romanian)": "Carmen",
+    "Maxim (Russian)": "Maxim",
+    "Tatyana (Russian)": "Tatyana",
+    "Enrique (Spanish, European)": "Enrique",
+    "Conchita (Spanish, European)": "Conchita",
+    "Mia (Spanish, Mexican)": "Mia",
+    "Astrid (Swedish)": "Astrid",
+    "Filiz (Turkish)": "Filiz",
+    "Gwyneth (Welsh)": "Gwyneth",
+    "Carter (English, American)": "en-US-Wavenet-A",
+    "Paul (English, American)": "en-US-Wavenet-B",
+    "Evelyn (English, American)": "en-US-Wavenet-C",
+    "Liam (English, American)": "en-US-Wavenet-D",
+    "Jasmine (English, American)": "en-US-Wavenet-E",
+    "Madison (English, American)": "en-US-Wavenet-F",
+    "Mark (English, American)": "en-US-Standard-B",
+    "Vanessa (English, American)": "en-US-Standard-C",
+    "Zachary (English, American)": "en-US-Standard-D",
+    "Audrey (English, American)": "en-US-Standard-E",
+    "Layla (English, British)": "en-GB-Standard-A",
+    "Ali (English, British)": "en-GB-Standard-B",
+    "Scarlett (English, British)": "en-GB-Standard-C",
+    "Oliver (English, British)": "en-GB-Standard-D",
+    "Bella (English, British)": "en-GB-Wavenet-A",
+    "John (English, British)": "en-GB-Wavenet-B",
+    "Victoria (English, British)": "en-GB-Wavenet-C",
+    "Ron (English, British)": "en-GB-Wavenet-D",
+    "Zoe (English, Australian)": "en-AU-Standard-A",
+    "Luke (English, Australian)": "en-AU-Standard-B",
+    "Samantha (English, Australian)": "en-AU-Wavenet-A",
+    "Steve (English, Australian)": "en-AU-Wavenet-B",
+    "Courtney (English, Australian)": "en-AU-Wavenet-C",
+    "Jayden (English, Australian)": "en-AU-Wavenet-D",
+    "Ashleigh (English, Australian)": "en-AU-Standard-C",
+    "Daniel (English, Australian)": "en-AU-Standard-D",
+    "Anushri (English, Indian)": "en-IN-Wavenet-A",
+    "Sundar (English, Indian)": "en-IN-Wavenet-B",
+    "Satya (English, Indian)": "en-IN-Wavenet-C",
+    "Sonya (Afrikaans)": "af-ZA-Standard-A",
+    "Aisha (Arabic)": "ar-XA-Wavenet-A",
+    "Ahmad 1 (Arabic)": "ar-XA-Wavenet-B",
+    "Ahmad 2 (Arabic)": "ar-XA-Wavenet-C",
+    "Nikolina (Bulgarian)": "bg-bg-Standard-A",
+    "Li Na (Chinese, Mandarin)": "cmn-CN-Wavenet-A",
+    "Wang (Chinese, Mandarin)": "cmn-CN-Wavenet-B",
+    "Bai (Chinese, Mandarin)": "cmn-CN-Wavenet-C",
+    "Mingli (Chinese, Mandarin)": "cmn-CN-Wavenet-D",
+    "Silvia (Czech)": "cs-CZ-Wavenet-A",
+    "Marie (Danish)": "da-DK-Wavenet-A",
+    "Annemieke (Dutch)": "nl-NL-Standard-A",
+    "Eva (Dutch)": "nl-NL-Wavenet-A",
+    "Lars (Dutch)": "nl-NL-Wavenet-B",
+    "Marc (Dutch)": "nl-NL-Wavenet-C",
+    "Verona (Dutch)": "nl-NL-Wavenet-D",
+    "Lotte (Wavenet) (Dutch)": "nl-NL-Wavenet-E",
+    "Tala (Filipino (Tagalog))": "fil-PH-Wavenet-A",
+    "Marianne (Finnish)": "fi-FI-Wavenet-A",
+    "Yvonne (French)": "fr-FR-Standard-C",
+    "Gaspard (French)": "fr-FR-Standard-D",
+    "Emilie (French)": "fr-FR-Wavenet-A",
+    "Marcel (French)": "fr-FR-Wavenet-B",
+    "Brigitte (French)": "fr-FR-Wavenet-C",
+    "Simon (French)": "fr-FR-Wavenet-D",
+    "Juliette (French, Canadian)": "fr-CA-Standard-A",
+    "Felix (French, Canadian)": "fr-CA-Standard-B",
+    "Camille (French, Canadian)": "fr-CA-Standard-C",
+    "Jacques (French, Canadian)": "fr-CA-Standard-D",
+    "Karolina (German)": "de-DE-Standard-A",
+    "Albert (German)": "de-DE-Standard-B",
+    "Angelika (German)": "de-DE-Wavenet-A",
+    "Oskar (German)": "de-DE-Wavenet-B",
+    "Nina (German)": "de-DE-Wavenet-C",
+    "Sebastian (German)": "de-DE-Wavenet-D",
+    "Thalia (Greek)": "el-GR-Wavenet-A",
+    "Sneha (Hindi)": "hi-IN-Wavenet-A",
+    "Arnav (Hindi)": "hi-IN-Wavenet-B",
+    "Aadhav (Hindi)": "hi-IN-Wavenet-C",
+    "Ishtevan (Hungarian)": "hu-HU-Wavenet-A",
+    "Helga (Icelandic)": "is-is-Standard-A",
+    "Anisa (Indonesian)": "id-ID-Wavenet-A",
+    "Budi (Indonesian)": "id-ID-Wavenet-B",
+    "Bayu (Indonesian)": "id-ID-Wavenet-C",
+    "Gianna (Italian)": "it-IT-Standard-A",
+    "Valentina (Italian)": "it-IT-Wavenet-A",
+    "Stella (Italian)": "it-IT-Wavenet-B",
+    "Alessandro (Italian)": "it-IT-Wavenet-C",
+    "Luca (Italian)": "it-IT-Wavenet-D",
+    "Koharu (Japanese)": "ja-JP-Standard-A",
+    "Miho (Japanese)": "ja-JP-Wavenet-A",
+    "Eiko (Japanese)": "ja-JP-Wavenet-B",
+    "Haruto (Japanese)": "ja-JP-Wavenet-C",
+    "Eichi (Japanese)": "ja-JP-Wavenet-D",
+    "Heosu (Korean)": "ko-KR-Standard-A",
+    "Grace (Korean)": "ko-KR-Wavenet-A",
+    "Juris (Latvian)": "lv-lv-Standard-A",
+    "Nora (Norwegian, Bokmål)": "nb-no-Wavenet-E",
+    "Malena (Norwegian, Bokmål)": "nb-no-Wavenet-A",
+    "Jacob (Norwegian, Bokmål)": "nb-no-Wavenet-B",
+    "Thea (Norwegian, Bokmål)": "nb-no-Wavenet-C",
+    "Aksel (Norwegian, Bokmål)": "nb-no-Wavenet-D",
+    "Amelia (Polish)": "pl-PL-Wavenet-A",
+    "Stanislaw (Polish)": "pl-PL-Wavenet-B",
+    "Tomasz (Polish)": "pl-PL-Wavenet-C",
+    "Klaudia (Polish)": "pl-PL-Wavenet-D",
+    "Beatriz (Portuguese, Portugal)": "pt-PT-Wavenet-A",
+    "Francisco (Portuguese, Portugal)": "pt-PT-Wavenet-B",
+    "Lucas (Portuguese, Portugal)": "pt-PT-Wavenet-C",
+    "Carolina (Portuguese, Portugal)": "pt-PT-Wavenet-D",
+    "Alice (Portuguese, Brazilian)": "pt-BR-Standard-A",
+    "Маша (Masha) (Russian)": "ru-RU-Wavenet-A",
+    "Илья (Ilya) (Russian)": "ru-RU-Wavenet-B",
+    "Алёна (Alena) (Russian)": "ru-RU-Wavenet-C",
+    "Пётр (Petr) (Russian)": "ru-RU-Wavenet-D",
+    "Aleksandra (Serbian)": "sr-rs-Standard-A",
+    "Eliska (Slovak)": "sk-SK-Wavenet-A",
+    "Rosalinda (Spanish, Castilian)": "es-ES-Standard-A",
+    "Elsa (Swedish)": "sv-SE-Standard-A",
+    "Zehra (Turkish)": "tr-TR-Standard-A",
+    "Yagmur (Turkish)": "tr-TR-Wavenet-A",
+    "Mehmet (Turkish)": "tr-TR-Wavenet-B",
+    "Miray (Turkish)": "tr-TR-Wavenet-C",
+    "Elif (Turkish)": "tr-TR-Wavenet-D",
+    "Enes (Turkish)": "tr-TR-Wavenet-E",
+    "Vladislava (Ukrainian)": "uk-UA-Wavenet-A",
+    "Linh (Vietnamese)": "vi-VN-Wavenet-A",
+    "Nguyen (Vietnamese)": "vi-VN-Wavenet-B",
+    "Phuong (Vietnamese)": "vi-VN-Wavenet-C",
+    "Viet (Vietnamese)": "vi-VN-Wavenet-D",
+    "Linda (English, Canadian)": "Linda",
+    "Heather (English, Canadian)": "Heather",
+    "Sean (English, Irish)": "Sean",
+    "Hoda (Arabic, Egypt)": "Hoda",
+    "Naayf (Arabic, Saudi Arabia)": "Naayf",
+    "Ivan (Bulgarian)": "Ivan",
+    "Herena (Catalan)": "Herena",
+    "Tracy (Chinese, Cantonese, Traditional)": "Tracy",
+    "Danny (Chinese, Cantonese, Traditional)": "Danny",
+    "Huihui (Chinese, Mandarin, Simplified)": "Huihui",
+    "Yaoyao (Chinese, Mandarin, Simplified)": "Yaoyao",
+    "Kangkang (Chinese, Mandarin, Simplified)": "Kangkang",
+    "HanHan (Chinese, Taiwanese, Traditional)": "HanHan",
+    "Zhiwei (Chinese, Taiwanese, Traditional)": "Zhiwei",
+    "Matej (Croatian)": "Matej",
+    "Jakub (Czech)": "Jakub",
+    "Guillaume (French, Switzerland)": "Guillaume",
+    "Michael (German, Austria)": "Michael",
+    "Karsten (German, Switzerland)": "Karsten",
+    "Stefanos (Greek)": "Stefanos",
+    "Szabolcs (Hungarian)": "Szabolcs",
+    "Andika (Indonesian)": "Andika",
+    "Heidi (Finnish)": "Heidi",
+    "Kalpana (Hindi)": "Kalpana",
+    "Hemant (Hindi)": "Hemant",
+    "Rizwan (Malay)": "Rizwan",
+    "Filip (Slovak)": "Filip",
+    "Lado (Slovenian)": "Lado",
+    "Valluvar (Tamil, India)": "Valluvar",
+    "Pattara (Thai)": "Pattara",
+    "An (Vietnamese)": "An",
+};
+const VOICE_LIST_ALT = Object.keys(VOICE_LIST).map(k => VOICE_LIST[k]);
 const palabrasSpam = ['join', 'joined', 'shared', 'LIVE', 'welcome'];
-const soundQueue = []; // Lista para almacenar los sonidos pendientes
-let isReading = false; // Variable para controlar si se está leyendo un mensaje o no
-let cache = []; // Lista en caché para almacenar los mensajes
+var voiceSelect = document.createElement('select');
+Object.keys(VOICE_LIST).forEach(function(key) {
+    var option = document.createElement('option');
+    option.text = key;
+    option.value = VOICE_LIST[key];
+    voiceSelect.appendChild(option);
+});
+console.log('Voz seleccionada:', voiceSelect.value);
+voiceSelect.addEventListener('change', function() {
+    fetchAudio(voiceSelect.value);
+});
+let isReading = false;
+let cache = [];
+let lastText = "";
+
+class Queue {
+    constructor() {
+        this.items = [];
+    }
+
+    enqueue(element) {
+        this.items.push(element);
+    }
+
+    dequeue() {
+        if (this.isEmpty()) {
+            return "Underflow";
+        }
+        return this.items.shift();
+    }
+
+    isEmpty() {
+        return this.items.length === 0;
+    }
+}
 
 function cacheMessage(text) {
-  cache.push(text);
-  if (cache.length > 15) {
-    cache.shift();
-  }
-} 
+    if (text.length >= 3 && text.length <= 400 && text !== lastText) {
+        cache.push(text);
+        lastText = text;
+    }
+    if (cache.length > 15) {
+        cache.shift();
+    }
+}
 
 function filtros(text) {
-  if (cache.includes(text)) {
-    return false;
-  }
-  if (palabrasSpam.some(text => text.includes(palabrasSpam))) {
-    return false;
-  }
-  cache.push(text);
-  if (cache.length > 15) {
-    cache.shift();
-  }
-  return true;
+    if (cache.includes(text) || palabrasSpam.some(word => text.includes(word))) {
+        return false;
+    }
+    cache.push(text);
+    if (cache.length > 15) {
+        cache.shift();
+    }
+    return true;
 }
-
-let lastSoundTime = 0;
-let lastDataTime = 0;
 
 function leerMensajes() {
-  if (cache.length > 0 && !isReading) {
-    const text = cache.shift();
-    hablarMensaje(text);
-  }
-}
-
-let ultimoMensaje = ""; // Variable para almacenar el último mensaje leído
-
-function hablarMensaje(text) {
-  const voiceSelect = document.querySelector("select");
-  const selectedVoice = voiceSelect.value;
-  const palabrasIgnorar = ["rose", "Heart", "GG", "@", "followed", "shared", "welcome"];
-
-  if (palabrasIgnorar.some(palabra => palabra.includes(text))) {
-    return;
-  }
-  if (text <= 3 || text > 300) {
-    return;
-  }
-  if (text === ultimoMensaje) { // Si el mensaje a leer es igual al último leído, no lo lea
-    return;
-  }
-  if (text) {
-    const messageLength = text.split(' ').length;
-    if (filtros(text)) {
-      const currentTime = Date.now();
-      const delay = messageLength > 5 ? 1000 : 2000;
-      let rate = 1.0;
-      if (messageLength > 20) {
-        const extraRate = Math.floor((messageLength - 20) / 20) * 0.1;
-        rate = Math.min(1.5, rate + extraRate);
-      }
-      if (responsiveVoice.isPlaying()) {
-        setTimeout(hablarMensaje, 100);
-        return;
-      }
-      try {
-        if (currentTime - lastSoundTime > delay && currentTime - lastDataTime > delay) {
-          lastSoundTime = currentTime;
-          ultimoMensaje = text; // Almacenar el último mensaje leído
-          responsiveVoice.speak(text, selectedVoice, {
-            rate: rate,
-            onend: function() {
-              setTimeout(() => {
-                leerMensajes();
-              }, 100);
-            }
-          });
-        } else {
-          setTimeout(() => {
-            hablarMensaje(text);
-          }, 100);
-        }
-      } catch (error) {
-        console.error('Error al hablar mensaje:', error);
-      }
+    if (cache.length > 0 && !isReading) {
+        const text = cache.shift();
+        fetchAudio(text);
     }
-  }
 }
-setInterval(leerMensajes, 100); // Añadir intervalo para leer mensajes cada segundo
-document.addEventListener('visibilitychange', function() {
-  if (document.visibilityState === 'hidden') {
-    responsiveVoice.cancel();
-  } else {
-    leerMensajes();
-  }
-});
+
+
+const readMessages = [];
+
+async function fetchAudio(txt) {
+    const delay = ms => new Promise(resolve => setTimeout(resolve, ms));
+    await delay(400);
+
+    try {
+        const resp = await fetch(TTS_API_ENDPOINT + makeParameters({ voice: voiceSelect.value, text: txt }));
+        if (resp.status !== 200) {
+            console.error("Mensaje incorrecto");
+            return;
+        }
+
+        const blob = await resp.blob();
+        const blobUrl = URL.createObjectURL(blob);
+        if (audioqueue) {
+            audioqueue.enqueue(blobUrl);
+            if (!isPlaying) kickstartPlayer();
+        }
+        const index = cache.indexOf(txt);
+        if (index !== -1) {
+            cache.splice(index, 1);
+        }
+
+        // Elimina el mensaje de cache una vez que se ha reproducido
+        setTimeout(() => {
+            URL.revokeObjectURL(blobUrl);
+        }, 10000); // Cambia el valor 10000 por el tiempo en milisegundos que deseas que dure el archivo temporal
+
+    } catch (error) {
+        console.error("Error:", error);
+    }
+}
+
+function makeParameters(params) {
+    return Object.keys(params)
+        .map(k => encodeURIComponent(k) + '=' + encodeURIComponent(params[k]))
+        .join('&');
+}
+
+function skipAudio() {
+    if (audio.paused) return console.error("skipped player while paused");
+    if (audioqueue.isEmpty()) {
+        isPlaying = false;
+        audio.pause();
+    } else {
+        isPlaying = true;
+        audio.src = audioqueue.dequeue();
+        audio.load();
+        audio.play();
+        audioqueue.dequeue();
+    }
+}
+
+function kickstartPlayer() {
+    if (audioqueue.isEmpty()) return isPlaying = false;
+    if (!audio.paused) return console.error("started player while running");
+    isPlaying = true;
+    audio.src = audioqueue.dequeue();
+    audio.load();
+    audio.play();
+    audioqueue.dequeue();
+    readMessages.shift();
+}
+
+window.onload = async function() {
+    try {
+        audio = document.getElementById("audio");
+        skip = document.getElementById("skip-button");
+        isPlaying = false;
+        audioqueue = new Queue();
+
+        if (skip) {
+            skip.onclick = skipAudio;
+        } else {
+            console.error("Error: skip-button is undefined");
+        }
+
+        if (audio) {
+            audio.addEventListener("ended", kickstartPlayer);
+        } else {
+            console.error("Error: audio is undefined");
+        }
+
+        leerMensajes();
+
+    } catch (error) {
+        console.error("Error:", error);
+    }
+};
